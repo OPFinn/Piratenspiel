87d279e27dd90010fe074eeb002915b9d7b467d3
OPFinn <finnlewe57@gmail.com>
2026-06-11 18:40:14 +0200

    Added new function for setting the animation
    Added new function to set a animation with a button

--- Piratenspiel/script.js
+++ Piratenspiel/script.js
@@ -1,11 +1,21 @@
 let frame = 0;
+let state = 'IDLE';
 
-
+setInterval(moveCharacter, 100); //Alle 100 Millisek. wiederholen
 
 function moveCharacter() {
-    pirate.src = `img/2/2_entity_000_IDLE_00${frame}.png`;
-    frame++;
+    pirate.src = `img/2/2_entity_000_${state}_00${frame}.png`;
+    /* ruft das Image auf mit der Variable state und setzt die
+    Bilder Nummer auf frame */
+    frame++; // frame + 1
  if(frame === 7) {
-    frame = 0;
+    frame = 0; // wenn frame = 7, wird es wieder auf 0 gesetzt
  }
+}
+
+function setState(newState)/* Der Parameter, also newState bekommt den Wert der
+dafuer festgelegt wird z.b. bei onclick*/ {
+   frame = 0;
+   state = newState; /* Dann wird state zu newState also zum 
+   Wert vom newState */
 }
